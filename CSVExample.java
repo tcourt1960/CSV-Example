e0aed11083943ee59f484de0b7d48b0bef6e1abf
Tom Courtney <tpcourtney@gmail.com>
2017-01-18 12:59:45 -0500

    First commit

---
+++ CSVExample.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of CSVExample here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import edu.duke.*;
+import org.apache.commons.csv.*;
+
+public class CSVExample {
+public void countryinfo(CSVParser parser, String country){
+//returns string of info about the country or returns "not Found"
+
+for (CSVRecord record : parser) {
+    String if (
+System.out.println(country + " : " + Exports + Value (dollars));
+
+}
+
+    for(CSVRecord record : parser){
+        System.out.println(record.get("Account"));
+    }
+    
+    
+}
+
+public void tester {
+    FileResource fr= new FileResource();
+    CSVParser parser = fr.getCSVParser();
+    
+}
+
+}
