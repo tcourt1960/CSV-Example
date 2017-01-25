ce8e079e0179499833b451cce03b753d1ce0fba9
Tom Courtney <tpcourtney@gmail.com>
2017-01-25 09:18:47 -0500

    Got first section working - look up country and print exports and value for that country

--- CSVExample.java
+++ CSVExample.java
@@ -9,26 +9,31 @@ import edu.duke.*;
 import org.apache.commons.csv.*;
 
 public class CSVExample {
-public void countryinfo(CSVParser parser, String country){
-//returns string of info about the country or returns "not Found"
+    
+    public void countryinfo(CSVParser parser, String country){
+        //returns string of info about the country or returns "not Found
+        
 
-for (CSVRecord record : parser) {
-    String if (
-System.out.println(country + " : " + Exports + Value (dollars));
-
-}
-
-    for(CSVRecord record : parser){
-        System.out.println(record.get("Account"));
+        for(CSVRecord record : parser){
+            String thecountry = record.get("Country");
+            
+            if(thecountry.contains(country)){
+                String exports = record.get("Exports");
+                String value = record.get("Value");
+                System.out.println(country + " : " + exports + " : " + value);
+            }
+        }
     }
     
+    public void tester() {
+        FileResource fr= new FileResource();
+        CSVParser parser = fr.getCSVParser();
     
-}
-
-public void tester {
-    FileResource fr= new FileResource();
-    CSVParser parser = fr.getCSVParser();
+        // be sure to reset parser after each method using
+        parser =fr.getCSVParser();
+        countryinfo(parser,"Germany");
     
-}
+    
+    }
 
 }
